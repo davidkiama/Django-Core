66733413bc06914c035261f7fac23c63ab38ba8f
davidkiama <davidkiama98@gmail.com>
2022-03-21 19:44:08 +0300

    Used a custom converter to get the date

--- The-Moringa-Tribune/news/urls.py
+++ The-Moringa-Tribune/news/urls.py
@@ -1,12 +1,17 @@
 
-from django.urls import path
+from django.urls import path, register_converter
 from .import views
+from .converters import DateConverter
+
+register_converter(DateConverter, 'date')
 
 
 # urlpatterns = [url('^$', views.welcome, name='welcome')]
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
-    path('today/', views.news_of_day, name='news-today')
+    path('today/', views.news_of_day, name='news-today'),
+    path('archives/<date:past_date>/',
+         views.past_days_news, name='pastNews')
 
 ]
--- The-Moringa-Tribune/news/views.py
+++ The-Moringa-Tribune/news/views.py
@@ -1,6 +1,8 @@
-from curses.ascii import HT
+
+from ast import Str
 import datetime as dt
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
 
 # Create your views here.
 
@@ -33,3 +35,31 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+
+def past_days_news(request, past_date):
+    # try:
+    #     # converts date from the string
+    #     date = dt.datetime.strftime(past_date, '%Y-%m-%d').date()
+    #     print('************************')
+    #     print(date)
+    # except:
+    #     # Raise error when valueError is thrown
+    #     raise Http404()
+
+    # check if the passed in object is truly a date
+    if past_date.year:
+        date = past_date
+    else:
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f''' 
+    <html>
+        <body>
+            <h1>News for {day} {date.day} - {date.month} - {date.year}  </h1>
+        </body>
+    </html>
+    '''
+
+    return HttpResponse(html)
