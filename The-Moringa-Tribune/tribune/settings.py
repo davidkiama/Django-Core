3b53d46923af31565fbdb55ad883fc994de159b1
davidkiama <davidkiama98@gmail.com>
2022-03-22 14:38:30 +0300

    Added static files

--- The-Moringa-Tribune/tribune/settings.py
+++ The-Moringa-Tribune/tribune/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news.apps.NewsConfig'
+    'news.apps.NewsConfig',
+    'bootstrap3'
+
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+import os
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
