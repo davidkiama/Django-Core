4e53f5f8b3c468cb0cd2e340a8cb4e85b65c2f15
davidkiama <davidkiama98@gmail.com>
2022-03-23 14:22:26 +0300

    Database Relationship

--- The-Moringa-Tribune/tribune/settings.py
+++ The-Moringa-Tribune/tribune/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune1',
+        'USER': 'kiama',
+        'PASSWORD': 'kiamapwd',
+        'HOST': 'localhost',
     }
 }
 
@@ -108,7 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
